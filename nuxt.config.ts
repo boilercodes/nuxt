2192f19a9e056d466392402fc21ed85e84d8f6ae
rmenai <rami.menai@outlook.com>
2022-04-27 15:08:00 +0300

    Add final template

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  srcDir: "src/",
+  modules: [
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "nuxt-windicss",
+    "unplugin-icons/nuxt",
+  ],
+  colorMode: {
+    classSuffix: "",
+  },
+});
