d548188c386e2b7f6722f96508c87dda4bdd2ade
rmenai <rami.menai@outlook.com>
2022-08-01 20:25:58 +0300

    Replace windicss with tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,9 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   srcDir: "src/",
-  buildModules: ["@pinia/nuxt", "@vueuse/nuxt", "nuxt-windicss"],
-  modules: ["@nuxtjs/color-mode"],
+  buildModules: ["@pinia/nuxt", "@vueuse/nuxt"],
+  modules: ["@nuxtjs/color-mode", "@nuxtjs/tailwindcss"],
 
   colorMode: { classSuffix: "" },
+  tailwindcss: { cssPath: "~/assets/css/main.scss" },
 });
