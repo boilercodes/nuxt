7a763b45f43b9c6d71d486b85a8bdb4df2bbd9e7
rmenai <rami.menai@outlook.com>
2022-12-09 22:32:02 +0300

    Add i18n localisation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   srcDir: "src",
   imports: { dirs: ["stores"] },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  modules: ["@nuxtjs/i18n", "@nuxtjs/tailwindcss", "@pinia/nuxt", "@vueuse/nuxt"],
+
+  i18n: {
+    detectBrowserLanguage: { cookieKey: "locale" },
+    langDir: "assets/locales",
+    defaultLocale: "en",
+    locales: [{ code: "en", iso: "en-US", name: "English", file: "en.json" }],
+  },
 });
--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,31 @@
+<script setup lang="ts">
+const head = useLocaleHead({
+  addDirAttribute: true,
+  identifierAttribute: "id",
+  addSeoAttributes: true,
+});
+
+const route = useRoute();
+const { t } = useI18n();
+
+const pageLayout = `layout.${route.meta.layout ?? "default"}.titleTemplate`;
+const pageTitle = `page.${route.meta.title ?? "unknown"}.title`;
+
+// Dynamically change the page title based on the locale.
+const title = computed(() => t(pageLayout, { title: t(pageTitle) }));
+</script>
+
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
+    <Head>
+      <Title>{{ title }}</Title>
+      <Link v-for="link in head.link" :key="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
+      <Meta v-for="meta in head.meta" :key="meta.id" :property="meta.property" :content="meta.content" />
+    </Head>
+    <Body>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </Body>
+  </Html>
 </template>
